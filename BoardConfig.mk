whswhs1128/device_generic_common

commit 66fd14a317cbada5d7f05482504788839b26d9bf

Always use 64-bit binder

BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_EXTRA_KERNEL_MODULES := tp_smapi
 ifneq ($(filter efi_img,$(MAKECMDGOALS)),)
 TARGET_KERNEL_ARCH ?= x86_64
 endif
-TARGET_USES_64_BIT_BINDER := $(if $(filter x86_64,$(TARGET_ARCH) $(TARGET_KERNEL_ARCH)),true)
+TARGET_USES_64_BIT_BINDER := true
 
 BOARD_USES_GENERIC_AUDIO ?= false
 BOARD_USES_ALSA_AUDIO ?= true

commit 8352a5893403ced1554ca1bab8977b81ceeac273

BoardConfig.mk: set vmalloc=192M kernel cmdline
Needeed to avoid lack of vma, for simplicity also in x86_64 image
Fixes: 66fd14a317c ("Always use 64-bit binder")

BoardConfig.mk
@@ -79,7 +79,7 @@ ifneq ($(strip $(BOARD_GPU_DRIVERS)),)
 TARGET_HARDWARE_3D := true
 endif
 
-BOARD_KERNEL_CMDLINE := root=/dev/ram0 androidboot.selinux=permissive $(if $(filter true,$(TARGET_USES_64_BIT_BINDER)),,vmalloc=192M)
+BOARD_KERNEL_CMDLINE := root=/dev/ram0 androidboot.selinux=permissive vmalloc=192M
 TARGET_KERNEL_DIFFCONFIG := device/generic/common/selinux_diffconfig
 
 COMPATIBILITY_ENHANCEMENT_PACKAGE := true